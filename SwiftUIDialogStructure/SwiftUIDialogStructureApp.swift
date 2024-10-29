c0592dfa56624f106a7886a4ea9833712b8cd038
asukim <asukim2020@gmail.com>
2024-10-29 15:35:08 +0900

    first commit

---
+++ SwiftUIDialogStructure/SwiftUIDialogStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDialogStructureApp.swift
+//  SwiftUIDialogStructure
+//
+//  Created by 김현구 on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDialogStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
